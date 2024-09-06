9d879001ab583998ebec27fb76b358b01b2c8a33
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-06 13:40:40 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Vortex;
+
+namespace VortexEditor;
+
+public static class VortexEditor
+{
+    public static void Main(string[] args)
+    {
+        Game.DefaultNamespace = "VortexEditor";
+        Game.Initialize(args);
+        
+        // TODO: Add initial scene
+
+        Game.Run();
+    }
+}
