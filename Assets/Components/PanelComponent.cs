576304f3eed7e3b72ac268873de90eee88f60aa1
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-07 12:51:26 +1000

    Added roboto fonts

---
+++ Assets/Components/PanelComponent.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Raylib_cs;
+using Vortex;
+
+namespace VortexEditor;
+
+public class PanelComponent : Component
+{
+    public override void Update(float dt)
+    {
+        base.Update(dt);
+    }
+
+    public override void Draw()
+    {
+        base.Draw();
+    }
+}
