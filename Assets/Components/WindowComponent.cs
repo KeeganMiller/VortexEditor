2a5eefe572c816f25e7e2bd898d1a00c9bc1ae3b
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-10 15:38:17 +1000

    Added drawing of the header and updating position

--- Assets/Components/WindowComponent.cs
+++ Assets/Components/WindowComponent.cs
@@ -22,6 +22,8 @@ public class WindowComponent : UIComponent
         }
     }
 
+    private Vector2 _headerPosition = Vector2.Zero;
+
     public override void Constructor(ResourceManager resources)
     {
         base.Constructor(resources);
@@ -31,6 +33,7 @@ public class WindowComponent : UIComponent
     public override void Update(float dt)
     {
         base.Update(dt);
+        _headerPosition = new Vector2(Owner.Transform.Position.X, Owner.Transform.Position.Y - 10);
     }
 
     public override void Draw()
@@ -38,17 +41,18 @@ public class WindowComponent : UIComponent
         base.Draw();
 
         if(Width > 0 && Height > 0)
-        {
-            var position = Owner.Transform.Position;
-            var scale = Owner.Transform.Scale;
-            Raylib.DrawRectangleRounded(new Rectangle(position, Width * scale.X, Height * scale.Y), 3, 0, new Color(120, 120, 120, 255));
-            Raylib.DrawRectangleRounded(new Rectangle(position, Width * scale.X, Height * scale.Y), 3, 0, new Color(56, 56, 56, 255));
+        {            
+            Raylib.DrawRectangleRec(new Rectangle(Owner.Transform.Position, Width * Owner.Transform.Scale.X, Height * Owner.Transform.Scale.Y), new Color(225, 225, 225, 255));
+            Raylib.DrawRectangleRounded(new Rectangle(_headerPosition, Width * Owner.Transform.Scale.X, _headerHeight * Owner.Transform.Scale.Y), 0.3f, 0, new Color(56, 56, 56, 255));
         }
+    }
+
     }
 
     private void CreateTextComponent()
     {
         _headerTextElement = new Element("HeaderTextElement");
+        Owner.Owner.AddElement(_headerTextElement);
         _headerText = new TextComponent();
         _headerText.ComponentId = Guid.NewGuid().ToString();
         _headerText.Name = "Window Header Text";
@@ -57,6 +61,6 @@ public class WindowComponent : UIComponent
         _headerTextElement.AddComponent(_headerText);
         _headerTextElement.SetParent(Owner);
         _headerTextElement.Transform.Position = new Vector2(5, 5);
-        Owner.Owner.AddElement(_headerTextElement);
+        
     }
 }
