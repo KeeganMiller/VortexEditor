1004f634d1e977df8dca1b5e8964dd31c0eea3c5
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-13 12:40:04 +1000

    Added resizing of the panels

--- Assets/Components/Panel/PanelComponent.cs
+++ Assets/Components/Panel/PanelComponent.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Numerics;
 using Raylib_cs;
 using Vortex;
@@ -16,14 +17,25 @@ public enum EPanelLocation
 
 public class PanelComponent : UIComponent
 {
+
+    public const float MIN_PANEL_WIDTH = 50;
+    public const float MAX_PANEL_WIDTH = 600;
+    public const float MIN_PANEL_HEIGHT = 50;
+    public const float MAX_PANEL_HEIGHT = 500;
+    
+    
+
     private EPanelLocation _panelLocation { get; set; } = EPanelLocation.PANEL_Left;
     private Color _panelColor { get; set; } = Color.Black;
     private PanelManager? _panelManager { get; set; }                     // Reference to the panel manager
     
     private float _toolbarHeight;
+
+    // == Resize Properties == //
     private float _resizeTollerence = 10;                                   // How far the mouse can be to start resizing
     private bool _isMouseOverEdge = false;
     private bool _isResizing = false;
+    private Vector2 _lastResizeMousePos;
 
     public override void Constructor(ResourceManager resourceManager)
     {
@@ -101,6 +113,22 @@ public class PanelComponent : UIComponent
         base.Update(dt);
         HandleMouseCursor();
         
+        if(IsMouseOver && IsMouseOverEdge())
+        {
+            if(Input.IsMouseButtonClicked(EMouseButton.MOUSE_Left))
+            {
+                _isResizing = true;
+                _lastResizeMousePos = Input.GetMousePosition(false);
+            }
+        }
+
+        if(_isResizing && Input.IsMouseButtonReleased(EMouseButton.MOUSE_Left))
+        {
+            _isResizing = false;
+        }
+
+        if(_isResizing)
+            ResizePanel();
     }
 
     private void HandleMouseCursor()
@@ -134,6 +162,86 @@ public class PanelComponent : UIComponent
                 Raylib.SetMouseCursor(MouseCursor.Default);
             }
         }
+    }
+
+    private void ResizePanel()
+    {
+        var nextStep = Input.GetMousePosition(false) - _lastResizeMousePos;
+
+        switch(_panelLocation)
+        {
+            case EPanelLocation.PANEL_Left:
+                if(IsWithinBounds(nextStep))
+                    Width += nextStep.X;
+                break;
+            case EPanelLocation.PANEL_Right:
+                if(IsWithinBounds(nextStep))
+                    Width -= nextStep.X;
+                break;
+            case EPanelLocation.PANEL_Up:
+                if(IsWithinBounds(nextStep))
+                {
+                    Height += nextStep.Y;
+                    if(_panelManager != null)
+                    {
+                        _panelManager.PanelLeft.Height -= nextStep.Y;
+                        _panelManager.PanelRight.Height -= nextStep.Y;
+                    }
+                }
+                break;
+            case EPanelLocation.PANEL_Down:
+                if(IsWithinBounds(nextStep))
+                {
+                    Height -= nextStep.Y;
+                    if(_panelManager != null)
+                    {
+                        _panelManager.PanelLeft.Height += nextStep.Y;
+                        _panelManager.PanelRight.Height += nextStep.Y;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+        _lastResizeMousePos = Input.GetMousePosition(false);
+        SetOriginAndAnchor(Origin, Anchor);
+    }
+
+    private bool IsWithinBounds(Vector2 nextStep)
+    {
+        if(_panelManager == null)
+            return false;
+        
+        switch(_panelLocation)
+        {   
+            case EPanelLocation.PANEL_Left:
+                if(Width + nextStep.X <= MAX_PANEL_WIDTH && nextStep.X > 0)
+                    return true;
+                else if(Width + nextStep.X >= MIN_PANEL_WIDTH && nextStep.X < 0)
+                    return true;
+                else if(Width + nextStep.X > MIN_PANEL_WIDTH && Width + nextStep.X < MAX_PANEL_WIDTH)
+                    return true;
+                return false;
+            case EPanelLocation.PANEL_Right:
+                if(Width - nextStep.X <= MAX_PANEL_WIDTH && nextStep.X < 0)
+                    return true;
+                else if(Width - nextStep.X >= MIN_PANEL_WIDTH && nextStep.X > 0)
+                    return true;
+                else if(Width - nextStep.X > MIN_PANEL_WIDTH && Width - nextStep.X < MAX_PANEL_WIDTH)
+                    return true;
+                return false;
+            case EPanelLocation.PANEL_Down:
+                if(Height - nextStep.Y <= MAX_PANEL_HEIGHT && nextStep.Y < 0)
+                    return true;
+                else if(Height - nextStep.Y >= MIN_PANEL_HEIGHT && nextStep.Y > 0)
+                    return true;
+                else if(Height - nextStep.Y > MIN_PANEL_HEIGHT && Height - nextStep.Y < MAX_PANEL_HEIGHT)
+                    return true;
+                return false;
+        }
+
+        return false;
     }
 
     private bool IsMouseOverEdge()
